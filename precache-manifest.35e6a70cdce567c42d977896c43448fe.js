3d20dbdd3a90cdc71442adbc99aae8e4d4689a0f
IFrame <san4uk777@gmail.com>
2020-10-30 20:24:27 +0300

    Updates

---
+++ precache-manifest.35e6a70cdce567c42d977896c43448fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64032d8bd18623e4e6470c8fd21028c4",
+    "url": "/myocean-react/index.html"
+  },
+  {
+    "revision": "54eb952814e15fac4a39",
+    "url": "/myocean-react/static/css/4.8af33704.chunk.css"
+  },
+  {
+    "revision": "9d70102fe0b8aad3cb2b",
+    "url": "/myocean-react/static/css/5.884e9047.chunk.css"
+  },
+  {
+    "revision": "fda9ae2bd1470d4ad57d",
+    "url": "/myocean-react/static/css/main.b92b4ae5.chunk.css"
+  },
+  {
+    "revision": "a1f6f04a67b1ba75b715",
+    "url": "/myocean-react/static/js/2.f5c84478.chunk.js"
+  },
+  {
+    "revision": "b000f75dc44bfb1b5a2501d1b29bd254",
+    "url": "/myocean-react/static/js/2.f5c84478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d74a1c3410ebec3c15f",
+    "url": "/myocean-react/static/js/3.b89f3d0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myocean-react/static/js/3.b89f3d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54eb952814e15fac4a39",
+    "url": "/myocean-react/static/js/4.6951ba3e.chunk.js"
+  },
+  {
+    "revision": "9d70102fe0b8aad3cb2b",
+    "url": "/myocean-react/static/js/5.50074f94.chunk.js"
+  },
+  {
+    "revision": "fda9ae2bd1470d4ad57d",
+    "url": "/myocean-react/static/js/main.7babf7dc.chunk.js"
+  },
+  {
+    "revision": "e613f763cd0a7dfadc87",
+    "url": "/myocean-react/static/js/runtime-main.99bb864b.js"
+  }
+]);
